ad55e241b58e11acc0d72d0131e3521932274a65
abhinav8292 <39800202+abhinav8292@users.noreply.github.com>
2021-07-14 21:59:32 +0530

    Delete socialApp directory

--- socialApp/src/action/action.types.js
+++
@@ -1,5 +0,0 @@
-export const IS_AUTHENTICATED = 'IS_AUTHENTICATED';
-export const SET_USER = 'SET_USER';
-export const SET_POST = 'SET_POST';
-export const SET_USER_POST = 'SET_USER_POST';
-export const ERROR_POST = 'ERROR_POST';
--- socialApp/src/utils/options.js
+++
@@ -1,6 +0,0 @@
-export const options = {
-  mediaType: 'photo',
-  maxWidth: 300,
-  maxHeight: 550,
-  quality: 1,
-};
